68f6cab503ff56aba9e8597588cf0e067749770f
Sarim Malik <sarimrasheed@gmail.com>
2021-08-03 14:34:05 -0400

    Fixed About page

--- pages/about.js
+++ pages/about.js
@@ -12,22 +12,23 @@ export default function about() {
       <div className="font-bold text-black">About</div>
       <div className="mt-3">Hi friends 👋🏽 </div>
       <div className="mt-2">
-        I'm Sarim and I use this space to collect stuff that I find interesting.
-        My vision for this space is to function as a personal collection that
-        can outlive me. I'd like to build a digital identity of sorts that I can
-        use for reflection while I'm alive and leave it as a time capsule for
-        others to follow.
+        I'm Sarim and I use this space to collect things (articles, quotes,
+        projects) that I find interesting. My vision for this space is to
+        function as a personal collection that can outlive me. I'd like to build
+        a digital identity of sorts that I can use for reflection and leave it
+        as a time capsule.
       </div>
       <div className="mt-2">
         For context, I am a mechanical engineering grad from{" "}
         <a href="https://www.mcgill.ca/" target="_blank">
           McGill University
         </a>
-        . Currently, I am building{" "}
+        . For the past 5 years I’ve lived in Montreal, Canada and recently moved
+        to Toronto where I am currently building {""}
         <a href="https://neat.run/" target="_blank">
           Neat
         </a>{" "}
-        with my friends from college. In the past, I've done data-related roles
+        with my friends from college. In the past, I've done data-centric roles
         at{" "}
         <a href="https://www.ea.com/" target="_blank">
           Electronic Arts (EA)
@@ -45,14 +46,14 @@ export default function about() {
       <div className="mt-2">
         These days I am brainstorming personal projects at the intersection of
         hardware and software. In my spare time, you'll find me cloning projects
-        off Github and reverse-engineering them. Not the most efficient way to
-        learn but it works really well for me, especially in terms of speed.
+        off of Github and reverse-engineering them. Not the most efficient way
+        to learn but works really well for me, especially in terms of speed.
         Long term, I'd like to build the muscle memory that some prodigy devs
-        have. Feel free to ping me about anything you found interesting here.
-        Always happy to chat.
+        have.
       </div>
       <div className="mt-2">
-        If you'd like to contact me, check out the{" "}
+        Feel free to ping me about anything you find interesting here. Always
+        happy to chat. If you'd like to reach out, check out the{" "}
         <Link key={"Contact"} href={"/contact"}>
           contact
         </Link>{" "}
@@ -67,6 +68,12 @@ export default function about() {
         alt="Picture of author"
         className="flex items-center"
       />
+      <div className="text-xs flex">
+        <div className="font-bold">Note</div>
+        <div className="ml-1">
+          Thanks to my friend, Zara Amer, for reviewing drafts of this page.
+        </div>
+      </div>
     </div>
   );
 }
