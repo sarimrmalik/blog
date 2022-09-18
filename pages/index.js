388140a34b0e07e701c59ba57974bcc53259c883
Sarim Malik <sarimrasheed@gmail.com>
2022-09-18 00:00:55 -0400

    Add styling to notes

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,10 @@ export default function Home({ blogs }) {
                   <div key={i} className="flex w-full justify-between py-2">
                     <div className="flex gap-8">
                       <p className="opacity-60">{`0${i + 1}`}</p>
-                      <button onClick={() => router.push(`/${blog.slug}`)}>
+                      <button
+                        className="text-blue-500 hover:text-blue-600 hover:underline text-lg"
+                        onClick={() => router.push(`/${blog.slug}`)}
+                      >
                         {blog.title}
                       </button>
                     </div>
